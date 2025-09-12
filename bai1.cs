d60e28b08adc984b0e3c668699655774a70c5ecc
dtc2154801030027 <dtc1254801030027@ictu.edu.vn>
2025-09-12 10:07:32 +0700

    sua

--- bai1.cs
+++ bai1.cs
@@ -7,6 +7,7 @@ import java.util.*;
 public class DanhSachQuanLy {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // sdfdss
 
         // Danh sách dữ liệu: lưu sinh viên, giáo viên, môn học, đăng ký, điểm...
         ArrayList<String> students = new ArrayList<String>();
